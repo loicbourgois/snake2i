d472b87bc850220a4956eadcfd31dc6c09ebf836
loicbourgois <loicbourgois.web@gmail.com>
2015-11-17 15:18:29 +0100

    Add click to go to options

--- main-window.cpp
+++ main-window.cpp
@@ -5,10 +5,18 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    optionsForm = new OptionsForm();
     ui->setupUi(this);
+    QObject::connect(this->ui->pushButtonOptions, SIGNAL(clicked()),
+                         this, SLOT(goToOptions()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::goToOptions()
+{
+    this->setCentralWidget(optionsForm);
 }
--- main-window.h
+++ main-window.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "options-form.h"
 #include <QMainWindow>
 
 namespace Ui {
@@ -14,9 +15,12 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+public slots:
+    void goToOptions();
 
 private:
     Ui::MainWindow *ui;
+    OptionsForm * optionsForm;
 };
 
 #endif // MAINWINDOW_H
